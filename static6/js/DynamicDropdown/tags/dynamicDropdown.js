2a9ca3ad8915a5c7f173a15ec0a1f79f9bddb6ec
thomas <thomas@weblizards.de>
2016-07-06 16:45:01 +0200

    Made the object input field working again. TODO: everything else, like grid handling

--- static6/js/DynamicDropdown/tags/dynamicDropdown.js
+++ static6/js/DynamicDropdown/tags/dynamicDropdown.js
@@ -1,30 +1,14 @@
 /**
- * Pimcore
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.pimcore.org/license
  *
  * @copyright  Copyright (c) 2016 Weblizards GmbH (http://www.weblizards.de)
  * @author     Thomas Keil <thomas@weblizards.de>
- * @license    http://www.pimcore.org/license
+ * @license    GPLv3
  */
 
 pimcore.registerNS("pimcore.object.tags.dynamicDropdown");
 pimcore.object.tags.dynamicDropdown = Class.create(pimcore.object.tags.select, {
 
     type: "dynamicDropdown",
-
-    initialize: function (data, layoutConf) {
-        this.data = data;
-        this.fieldConfig = layoutConf;
-        this.mode = 'remote';
-
-        this.fieldConfig.index = 15005;
-    },
 
     getGridColumnEditor:function (field) {
         return null;
@@ -35,31 +19,28 @@ pimcore.object.tags.dynamicDropdown = Class.create(pimcore.object.tags.select, {
     },
     
     getLayoutEdit: function () {
-        // generate store
-        options_store = new Ext.data.Store({
+
+        this.options_store = new Ext.data.JsonStore({
             proxy: {
                 type: 'ajax',
-                url: '/plugin/DynamicDropdown/dynamicdropdown/options',
-                reader: {
-                    type: "json"
-                } // , [{name: 'value'}, {name: 'key'}])
-
+                url: '/plugin/DynamicDropdown/dynamicdropdown/options'
             },
-            baseParams: {
+            fields: ["key", "value"],
+            listeners: {
+                "load": function(/* store */) {
+                    this.component.setValue(this.data);
+                }.bind(this)
+            }
+        });
+
+        this.options_store.load({
+            params: {
                 source_parent: this.fieldConfig.source_parentid,
                 source_methodname: this.fieldConfig.source_methodname,
                 source_classname: this.fieldConfig.source_classname,
                 source_recursive: this.fieldConfig.source_recursive,
                 current_language: pimcore.settings.language,
                 sort_by: this.fieldConfig.sort_by
-            },
-            autoLoad: true,
-            loading: true,
-            listeners: {
-                "load": function(store) {
-                    console.log(this);
-                    this.component.setValue(this.data);
-                }.bind(this)
             }
         });
 
@@ -71,7 +52,7 @@ pimcore.object.tags.dynamicDropdown = Class.create(pimcore.object.tags.select, {
             forceSelection: true,
             selectOnFocus: true,
             fieldLabel: this.fieldConfig.title,
-            store: options_store,
+            store: this.options_store,
             itemCls: "object_field",
             width: 300,
             displayField: "key",
